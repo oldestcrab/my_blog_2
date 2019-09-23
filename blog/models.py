c776bc13d6b88a0fcba34694143e553dcdc4a4dc
oldestcrab <oldestcrab@gmail.com>
2019-09-23 21:19:53 +0800

    blog应用，编写模型类

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class BlogType(models.Model):
+    type_name = models.CharField(max_length=15, verbose_name='博客分类')
+
+    def __str__(self):
+        return self.type_name
+
+    class Meta:
+        verbose_name = '博客分类'
+        verbose_name_plural = verbose_name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50, verbose_name='标题')
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='作者')
+    content = models.TextField(verbose_name='内容')
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING, verbose_name='博客分类')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+    last_update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+    is_delete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '博客:'.format(self.title)
+
+    class Meta:
+        verbose_name = '博客'
+        verbose_name_plural = verbose_name
