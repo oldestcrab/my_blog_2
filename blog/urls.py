f15c8341fd56a00ce0fa3f805e66b5ba87d46908
oldestcrab <oldestcrab@gmail.com>
2019-09-24 14:59:32 +0800

    配置博客列表页，分类页，详情页相关视图url

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('<int:blog_id>/', views.blog_detail, name='blog_detail'),
-    path('type/<int:blog_with_type_id>/', views.blog_with_type, name='blog_with_type'),
+    path('', views.blog_list, name='blog_list'),
+    path('<int:blog_id>', views.blog_detail, name='blog_detail'),
+    path('type/<int:blog_with_type_id>', views.blog_with_type, name='blog_with_type'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,21 @@ from django.shortcuts import get_object_or_404
 
 
 def blog_list(request):
+    # 所有博客
     blogs = Blog.objects.all()
+    # 所有博客分类
+    blog_types = BlogType.objects.all()
 
     context = {
         'blogs': blogs,
+        'blog_types': blog_types,
     }
 
     return render(request, 'blog/blog_list.html', context=context)
 
 
 def blog_detail(request, blog_id):
+    # 通过id获取博客对象
     blog = get_object_or_404(Blog, id=blog_id)
 
     context = {
@@ -23,10 +28,17 @@ def blog_detail(request, blog_id):
     return render(request, 'blog/blog_detail.html', context=context)
 
 def blog_with_type(request, blog_with_type_id):
+    # 当前博客分类
     blog_type = get_object_or_404(BlogType, id=blog_with_type_id)
+    # 当前博客分类的所有博客
     blogs = Blog.objects.filter(blog_type=blog_type)
+    # 所有博客分类
+    blog_types = BlogType.objects.all()
+
     context = {
         'blogs': blogs,
+        'blog_type': blog_type,
+        'blog_types': blog_types,
     }
 
     return render(request, 'blog/blog_with_type.html', context=context)
